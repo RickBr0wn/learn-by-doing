759ad814260c46e0f40fbd16b10f4bbd3df0afbd
Rick Brown <ricky.brown.00@gmail.com>
2020-09-01 16:37:53 +0100

    adds sounds

---
+++ Learn by Doing/PlaySounds.swift
@@ -0,0 +1,23 @@
+//
+//  PlaySounds.swift
+//  Learn by Doing
+//
+//  Created by Rick Brown on 01/09/2020.
+//  Copyright © 2020 Rick Brown. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+  if let path = Bundle.main.path(forResource: sound, ofType: type) {
+    do {
+      audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+      audioPlayer?.play()
+    } catch {
+      print("Could not find and play the sound file.")
+    }
+  }
+}
